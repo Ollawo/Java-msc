364d4b5f35f95731494674f6dc2673254f104e18
Ollawo <g.olavo.n@outlook.com>
2021-06-30 19:36:55 -0300

    Add files via upload
    Segurança sem funcionar tentando com o spring 2.3

---
+++ deswebii-3/src/main/java/com/example/demo/Deswebii3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deswebii3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Deswebii3Application.class, args);
+	}
+
+}
