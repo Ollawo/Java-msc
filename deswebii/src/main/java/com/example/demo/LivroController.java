9a9f3fbde6ec855228fdcab7f9e7c449065917ea
Ollawo <g.olavo.n@outlook.com>
2021-06-30 19:35:05 -0300

    Add files via upload
    segurança não funciona

---
+++ deswebii/src/main/java/com/example/demo/LivroController.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LivroController {
+	
+	@Autowired
+	LivroRepository repository;
+	
+	@GetMapping("/livro")
+	public List<Livro> getAllLivros(){
+		return repository.findAll();
+	}
+	@GetMapping ("/livro/{id}")
+	public Livro getLivroPorId(@PathVariable Long id) {
+		return repository.findById(id).get();
+	}
+	@PostMapping("/livro")
+	public Livro saveLivro(@RequestBody Livro livro) {
+		return repository.save(livro);
+	}
+	@DeleteMapping("/livro/{id}")
+	//@PreAuthorize("hasRole('ADMIN')")
+	public void deleteLivro(@PathVariable Long id) {
+		repository.deleteById(id);
+	}
+	
+}
